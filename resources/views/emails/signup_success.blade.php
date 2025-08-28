8f8652942c8c4b334a51a974a2cd8ea9b1e9a9a1
KimHan2074 <ngothikimhan.2004@gmail.com>
2025-08-28 10:42:57 +0700

    fix send otp

---
+++ resources/views/emails/signup_success.blade.php
@@ -0,0 +1,7 @@
+<h3>Hello {{ $user->name }},</h3>
+<p>Your account has been successfully registered with our service.</p>
+<p>Your login details are as follows:</p>
+<ul>
+    <li>Full Name: {{ $user->name }}</li>
+    <li>Password: (secured, please remember it from registration)</li>
+</ul>
